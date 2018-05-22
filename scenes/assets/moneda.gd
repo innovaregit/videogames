2b8b53df0c1597d60ebd17ef8abb44db27b482ca
marceloagustini <agustinimarcelo@gmail.com>
2018-05-22 16:39:12 -0300

    3

---
+++ scenes/assets/moneda.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+onready var animacion = get_node("Sprite/animacion")
+var area
+
+func _ready():
+	area = get_node("area")
+	animacion.get_animation("gira").set_loop(true)
+	animacion.play("gira")
+	pass
+
+
+func _on_area_body_entered(body):
+	hide()
+	area.queue_free()
+	global.contador_monedas = global.contador_monedas + 1
+	print(global.contador_monedas)
+	pass # replace with function body
---
+++ scripts/global.gd
@@ -0,0 +1,2 @@
+extends Node
+export var contador_monedas = 0
